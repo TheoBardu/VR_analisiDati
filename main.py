4a03e2a90471abd838f1be84d72e45a01f0abde7
Theo Bardu <mattebardu@gmail.com>
2026-06-28 09:41:11 +0200

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 from analisi_datiVR import manager, analisi
 
 # La main directory deve essere la root (quindi la cartella dell'azienda)
-main_directory = '/Users/theo/Desktop/prova'
+main_directory = '/Users/theo/Desktop/P.IVA/Aziende/Ermes/Lavori/Ferrobac/1/rev/rev3/Rumore'
 misure_directory = '/misure'
 risultati_directory = '/output'
 dpi_directory = '/DPI_check'
@@ -15,47 +15,47 @@ dpi_directory = '/DPI_check'
 
 
 def main(main_directory):
-    # # sys.stdout = open('VR_rumore.out', 'w')
-    # chdir(main_directory + misure_directory) # mi sposto nella cartella delle misure
+    # sys.stdout = open('VR_rumore.out', 'w')
+    chdir(main_directory + misure_directory) # mi sposto nella cartella delle misure
     
-    # # ==========================================================================
-    # # Inizializzazione: lettura file e creazione tabelle =====================================
+    # ==========================================================================
+    # Inizializzazione: lettura file e creazione tabelle =====================================
 
-    # m = manager() # inizializzazione delle directory e dei file
-    # m.iterate_directory(file_name = 'dati.txt',format='csv') #iterazione su tutte le cartelle contenenti i dati .txt per salvare xls e csv
-    # # ==========================================================================
+    m = manager() # inizializzazione delle directory e dei file
+    m.iterate_directory(file_name = 'dati.txt',format='csv') #iterazione su tutte le cartelle contenenti i dati .txt per salvare xls e csv
+    # ==========================================================================
 
-    # # ==========================================================================
-    # # analisi dei dati acquisiti =====================================
+    # ==========================================================================
+    # analisi dei dati acquisiti =====================================
 
-    # data_folder = main_directory + misure_directory + '/data' 
-    # a = analisi(data_folder)
-    # df_avg = a.average_values() #creazione delle medie delle misure
-    # # # print(df_avg)
-    
-
-    # # Copio le info dei GrOm e Ti dal file excel
-    # df_HEG = a.get_scheda_info(df_avg, excel_info_dir=main_directory)     
-
-    # str_print_4input = '''
-    # §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
-    # !! -> Prima di Proseguire:\n
-    # Controlla il file averaged_data.csv e inserisci a mano i Ti e i GrOm.\n
-    # Poi premi invio per continuare con il codice...
-    # Puoi anche selezionare direttamente questa parte per continuare se hai interrotto il codice.\n
-    # §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
-    # '''
-    # input(str_print_4input)
-
-    
+    data_folder = main_directory + misure_directory + '/data' 
+    a = analisi(data_folder)
+    df_avg = a.average_values() #creazione delle medie delle misure
+    # # print(df_avg)
     
 
-    # # ==========================================================================
-    # # Valutazione Rischio 8 h =====================================
-    # # ==========================================================================
-    # a.analisi_8h(main_directory + risultati_directory, df_HEG)
+    # Copio le info dei GrOm e Ti dal file excel
+    df_HEG = a.get_scheda_info(df_avg, excel_info_dir=main_directory)     
+
+    str_print_4input = '''
+    §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+    !! -> Prima di Proseguire:\n
+    Controlla il file averaged_data.csv e inserisci a mano i Ti e i GrOm.\n
+    Poi premi invio per continuare con il codice...
+    Puoi anche selezionare direttamente questa parte per continuare se hai interrotto il codice.\n
+    §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+    '''
+    input(str_print_4input)
+
     
-    # input('Femi qui per ora, DPI da correggere.')
+    
+
+    # ==========================================================================
+    # Valutazione Rischio 8 h =====================================
+    # ==========================================================================
+    a.analisi_8h(main_directory + risultati_directory, df_HEG)
+    
+    input('Femi qui per ora, DPI da correggere.')
     
     data_folder = main_directory + misure_directory + '/data' 
     a = analisi(data_folder)
